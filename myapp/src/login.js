f257afd1b8e6e8154c4f6ce47917e00f25c5e7d4
Raeman's Projects <araiagent222@gmail.com>
2025-01-24 00:26:49 +0500

    Update login.js

--- myapp/src/login.js
+++ myapp/src/login.js
@@ -78,7 +78,7 @@ const Login = () => {
 
           <form onSubmit={handleSubmit}>
       <Button variant="primary" type="submit">
-               <Link style={{color:'black'}} to="/Home">Submit</Link>
+               <Link style={{color:'black'}} to="/">Submit</Link>
       </Button>
     </form>
         </Form>
